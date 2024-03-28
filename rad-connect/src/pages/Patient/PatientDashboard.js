ba6d7a3afd12d45cae439fdc45c1652e6f4da188
unknown <souviktechno98@gmail.com>
2024-03-28 13:26:57 +0530

    Patient Details Updated

--- rad-connect/src/pages/Patient/PatientDashboard.js
+++ rad-connect/src/pages/Patient/PatientDashboard.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "../Patient/PatientDashboard.css";
 import Navbar from "../../components/navbar/Navbar";
+import { useUserIdContext } from '../Common/UserIdContext';
 
 function PatientDashboard() {  
-  return (
+
+  const [patient, setPatient] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [age, setAge] = useState(null);
+  const { data } = useUserIdContext();
+
+  console.log(data);
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      console.log("Hi");
+        try {
+            const response = await fetch('http://localhost:8080/teleRadiology/getPatient', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: data })
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch patient');
+            }
+            const patientData = await response.json();
+            setPatient(patientData);
+            setLoading(false);
+            console.log(patientData);
+
+            const currentDate = new Date();
+            const birthDate = new Date(patientData.dateOfBirth);
+    
+            let yearsDiff = currentDate.getFullYear() - birthDate.getFullYear();
+            if (currentDate.getMonth() < birthDate.getMonth() || 
+              (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+               yearsDiff--;
+            }
+
+             setAge(yearsDiff);                            
+        } catch (error) {
+            console.error('Error fetching patient:', error);
+            setLoading(false);
+        }
+    };
+
+    fetchPatient();
+  },[data]);
+    
+    return (
     <section className="home">
       <Navbar/>
       <div className="container">
@@ -13,23 +60,26 @@ function PatientDashboard() {
           Patient
         </div>
         
-          <div className="personal-info">
-
-            <div className="patient-image">
-              <div className="image-circle-box"></div>
-              <div className="edit-option">Edit</div>
+        {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div className="personal-info">
+              <div className="patient-image">
+                <div className="image-circle-box"></div>
+                <div className="edit-option">Edit</div>
+              </div>
+              <div className="patient-info">
+                <p><span className="text-blue-extradark">{patient.firstName}&nbsp;{patient.lastName}</span></p>
+                <p>Email: <span className="text-blue-extradark">{patient.email}</span></p>
+                <p>Phone No: <span className="text-blue-extradark">{patient.phoneNumber}</span></p>
+                <p>Age: <span className="text-blue-extradark">{age}</span></p>
+                <p>Height: <span className="text-blue-extradark">{patient.height}</span></p>
+                <p>Weight: <span className="text-blue-extradark">{patient.weight}</span></p>
+                <p>Blood Group: <span className="text-blue-extradark">{patient.bloodGroup}</span></p>
+                <p>Gender: <span className="text-blue-extradark">{patient.gender}</span></p>
+              </div>
             </div>
-            <div className="patient-info">
-              <p><span className="text-blue-extradark">Dr Albert Einstein</span></p>
-              <p>Email <span className="text-blue-extradark">ttt@gmail.com</span></p>
-              <p>Phone No <span className="text-blue-extradark">9340677493</span></p>
-              <p>Age <span className="text-blue-extradark">23</span></p>
-              <p>Height <span className="text-blue-extradark">5'4</span></p>
-              <p>Weight <span className="text-blue-extradark">65</span></p>
-              <p>Blood Group <span className="text-blue-extradark">B-</span></p>
-              <p>Gender <span className="text-blue-extradark">Male</span></p>
-            </div>
-          </div>
+          )}
 
           <div className="other-info">
             <div className="medical">
